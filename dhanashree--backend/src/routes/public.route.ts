26c050d8f8301da892d986813229a388eb6bb13f
Sunilyogi333 <sunilnath0109@gmail.com>
2025-06-16 08:37:36 +0545

    fix: correct import path for publicController in public route

--- dhanashree--backend/src/routes/public.route.ts
+++ dhanashree--backend/src/routes/public.route.ts
@@ -1,6 +1,6 @@
 import type { Router as IRouter } from 'express'
 import Router from 'express'
-import publicController from '..//controllers/public/public.controller'
+import publicController from '../controllers/public/public.controller'
 import { catchAsync } from '../utils/catchAsync'
 
 const router: IRouter = Router()
