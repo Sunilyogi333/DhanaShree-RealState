cc54c00752eca3e5955cfa42299f3c293bae2de1
Sunilyogi333 <sunilnath0109@gmail.com>
2025-06-28 08:11:26 +0545

    fix: increase request limit to 1000 per minute in rate limiter

--- dhanashree--backend/src/utils/rateLimiter.utils.ts
+++ dhanashree--backend/src/utils/rateLimiter.utils.ts
@@ -2,8 +2,8 @@ import rateLimit from 'express-rate-limit'
 
 export const rateLimiter = rateLimit({
   windowMs: 60 * 1000, // per minute in milliseconds
-  max: 100,
-  message: 'You have exceeded the 100 requests in per minute limit!',
+  max: 1000,
+  message: 'You have exceeded the 1000 requests in per minute limit!',
   standardHeaders: true,
   legacyHeaders: false,
 })
